01126097deb76fe0775cd98ad691b84e3eb1c91a
HearneRichard <160042232+HearneRichard@users.noreply.github.com>
2024-02-18 10:05:31 -0500

    Add files via upload

---
+++ hi.py
@@ -0,0 +1,8 @@
+#In your  homework, you will get these values from the user
+num1 = 10
+num2 = 3
+
+print(num1, "-", num2, "=", num1 - num2)
+
+#Display a series of strings and variables
+print (num1, "squared is", num1**2)
